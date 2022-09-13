0eb507842eaad5c304fa81626a20e42357e902ea
Abhishek Bhattachaya <ab7607@rediffmail.com>
2022-09-13 11:36:01 +0530

    Initial phase2

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routesHandler = require('./routes/handler.js');
-
+const pool=require('./config/db.js');
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', routesHandler);
+
+//MySQL DB Connection
+pool.getConnection(err, conn) =>{
+
+    if(err) throw err;
+    const qry='INSERT INTO users(username, fullname, entrydate) VALUES(?,?,Now())';
+    conn.query(qry,)
+};
 
 const PORT = 4000; // backend routing port
 app.listen(PORT, () => {
